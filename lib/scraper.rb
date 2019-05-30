373bc53a025c39fd8778ca3a9bae669bde5f7791
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-30 23:04:14 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -16,7 +17,35 @@ class Scraper
     end
   end
   
+  def get_page 
+    #combined open-uri & nokigiri calls
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+  
+  def get_courses
+	  get_page.css(".post")
+	end
+	
+	# I need to iterate through get_courses
+	def make_courses
+	   get_courses.css(".post").each do |post|
+        course = Course.new
+        course.title = post.css("h2").text
+        course.schedule = post.css(".date").text
+        course.description = post.css("p").text
+      end
+	end
+	
+	def test 
+	 a = get_page.css(".post").first.css("h2").text
+	 b = get_page.css(".post").first.css(".date").text
+	 c = get_page.css(".post").first.css("p").text
+	 d = get_page.css(".post")
+	 puts d
+	end
+  
 end
 
 
-
+a = Scraper.new.test
+puts a
