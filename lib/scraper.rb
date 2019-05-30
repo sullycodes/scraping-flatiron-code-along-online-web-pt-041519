1d141f8bcf857a8c36346dc8360fba48deb22dd6
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-30 23:03:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,13 +20,6 @@ class Scraper
   def get_page 
     #combined open-uri & nokigiri calls
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-      
-      doc.css(".post").each do |post|
-        course = Course.new
-        course.title = post.css("h2").text
-        course.schedule = post.css(".date").text
-        course.description = post.css("p").text
-      end
   end
   
   def get_courses
